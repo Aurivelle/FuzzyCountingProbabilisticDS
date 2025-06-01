6d76e87f66e7baf6498bfe4900bd0ec775fe841d
謝博仲 <xiebozhong@Bryan.local>
2025-06-01 15:03:15 +0800

    bch9248 0601 autocorrector update

--- baseline/autoCorrector.py
+++ baseline/autoCorrector.py
@@ -1,11 +1,12 @@
 # autoCorrector.py
 from difflib import get_close_matches
 from fuzzyMatcher import FuzzyMatcher
+from top3 import Top3Suggester
 
 autocorrector = FuzzyMatcher()
 
 
-
+# dummy linear search for the closest word
 def get_correction(word, vocabulary, n=1, cutoff=0.8):
     """
     Returns the top candidate from vocabulary similar to the input word.
@@ -13,17 +14,24 @@ def get_correction(word, vocabulary, n=1, cutoff=0.8):
     candidates = get_close_matches(word, vocabulary, n=n, cutoff=cutoff)
     return candidates[0] if candidates else None
 
-# Simple simulate user input
-vocabulary = ["hello", "hello", "helo", "heloo"]
+# vocab_file = r"C:\Users\USER\Desktop\AdvDS FP\AdvDS-Final-Project\baseline\testcase\words_alpha.txt"
+# suggester = Top3Suggester(vocab_file)
 
-for text in vocabulary:
+# Simple simulate user input
+vocab_file = ["hello", "hello", "helo", "heloo"]
+suggester = Top3Suggester(vocab_file)
+
+for text in vocab_file:
     autocorrector.insert(text)
 
 
 # Query
 print("Should autocorrect 'helioo'? ", autocorrector.should_autocorrect("helioo"))
-suggestion = get_correction("helioo", vocabulary)
-print(f"Did you mean '{suggestion}'?")
+# suggestion = get_correction("helioo", vocabulary)
+if autocorrector.should_autocorrect("helioo"):
+    suggestion = suggester.suggest("helioo")
+    print(f"Suggestion: {suggestion[0]}")
+
 
 # Future AutoCorrection flow
 """
