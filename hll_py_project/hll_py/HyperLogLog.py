de9b53f4e19fca67fa35806d9dd2f705c3861802
謝博仲 <xiebozhong@Bryan.local>
2025-06-09 22:48:10 +0800

    bch9248

--- hll_py_project/hll_py/HyperLogLog.py
+++ hll_py_project/hll_py/HyperLogLog.py
@@ -2,22 +2,18 @@ import math
 from typing import List
 from .Hasher import str_to_u64
 
-
 class SketchConfig:
-    def __init__(
-        self, b=4, m=4, use_bias_correction=True, alpha_override=-1.0, epsilon=4.0
-    ):
+    def __init__(self, b=4, m=4, use_bias_correction=True, alpha_override=-1.0, epsilon=4.0):
         self.b = b
         self.m = m
         self.use_bias_correction = use_bias_correction
         self.alpha_override = alpha_override
         self.epsilon = epsilon
 
-
 class HyperLogLog:
     def __init__(self, cfg=SketchConfig()):
         self.cfg = cfg
-        self.m = 2**self.cfg.b
+        self.m = 2 ** self.cfg.b
         self.alpha_m = self.compute_alpha()
         self.registers = [0] * self.m
 
@@ -34,11 +30,12 @@ class HyperLogLog:
             return 0.7213 / (1 + 1.079 / self.m)
 
     def rho(self, w_suffix):
-        return (
-            w_suffix.bit_length() - w_suffix.bit_length() + 1
-            if w_suffix == 0
-            else 64 - w_suffix.bit_length() + 1
-        )
+        if w_suffix == 0:
+            return 64
+        return (w_suffix.bit_length() - 1) ^ 63
+        # return (w_suffix.bit_length() - w_suffix.bit_length() + 1
+        #         if w_suffix == 0 else
+        #         64 - w_suffix.bit_length() + 1)
 
     def insert(self, value):
         if isinstance(value, str):
@@ -56,15 +53,15 @@ class HyperLogLog:
             self.registers[i] = max(self.registers[i], other.registers[i])
 
     def estimate(self):
-        Z = sum([2**-r for r in self.registers])
-        E = self.alpha_m * self.m**2 / Z
+        Z = sum([2 ** -r for r in self.registers])
+        E = self.alpha_m * self.m ** 2 / Z
         V = self.registers.count(0)
-        if E <= 5 / 2 * self.m:
+        if E <= 5/2 * self.m:
             return self.m * math.log(self.m / V) if V != 0 else E
-        elif E <= 1 / 30 * (1 << 64):
+        elif E <= 1/30 * (1 << 64):
             return E
         else:
-            return -(1 << 64) * math.log(1 - E / (1 << 64))
+            return - (1 << 64) * math.log(1 - E / (1 << 64))
 
     def reset(self):
         self.registers = [0] * self.m
